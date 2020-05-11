f481467c596a1ffb1356ccaa127776b638c69fac
Ralph Magbanua <rmagbanua09@gmail.com>
2020-05-10 23:19:18 -0700

    Employee and Engineer Test PASSED

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,9 +1,25 @@
 // TODO: Write code to define and export the Employee class
-class Emplpoyee {
+class Employee {
     constructor(name, id, email) {
       this.name = name;
       this.id = id;
       this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id
+    }
+    
+    getEmail() {
+        return this.email
+    }
+
+    getRole() {
+        return "Employee"
     }
   }
   
